a06c6ba3c36714de4fab821213e9d62d3ee63944
Viktor Govže <viktor.govze@gmail.com>
2024-08-04 23:35:28 +0200

    added latex plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -349,10 +349,9 @@ require("lazy").setup({
   -- configuration, etc from `lua/custom/plugins/*.lua`
   -- This is the easiest way to modularize your config.
 
-  --  Uncomment the following line and add your plugins to
-  --  `lua/custom/plugins/*.lua` to get going.
   --  See `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  -- [[ Latex ]]
+  -- require("custom.plugins.latex")
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will
---
+++ nvim/lua/custom/plugins/latex.lua
@@ -0,0 +1,7 @@
+return {
+  "lervag/vimtex",
+  init = function()
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_general_viewer = "okular"
+  end
+}
